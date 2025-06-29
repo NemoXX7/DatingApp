b9cf94447f724be9497356a82685cd44a9792c48
Jay Steynberg <jaridsteynberg@gmail.com>
2025-06-29 13:43:13 +0200

    End of Section 5

---
+++ client/src/app/register/register.ts
@@ -0,0 +1,30 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Account } from '../_services/account';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+  private accountService = inject(Account);
+  cancelRegister = output<boolean>();
+  model: any = {}
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
