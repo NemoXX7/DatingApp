54e5217385ccdc06fc70c56900681bfec459f99a
Jay Steynberg <jaridsteynberg@gmail.com>
2025-09-02 18:35:57 +0200

    End of Section 16

--- client/src/app/nav/nav.ts
+++ client/src/app/nav/nav.ts
@@ -4,11 +4,12 @@ import { Account } from '../_services/account';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { HasRole } from '../_directives/has-role';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, HasRole],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
